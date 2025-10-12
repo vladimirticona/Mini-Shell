8bc972efd7ac2e6a2e9e4853169e917e7ac63501
Vladimir Ticona <vladimirticona369@gmail.com>
2025-10-11 23:45:23 -0500

    Implementacion de librerias que usaremos

---
+++ minishell.cpp
@@ -0,0 +1,20 @@
+//Implementacion de librerias que usaremos
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <cstring>
+#include <cstdlib>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+using namespace std;
+
+int main(){
+
+
+    return 0;
+}
